306364895c9aed9782f44d6c86b152382c1763bc
thatblindgeye <thatblindgeye@gmail.com>
2020-10-21 16:36:35 -0400

    Rewrite html and css, add image folder
    Rewrote the html and css to make the project responsiveness work better,
    and added an image folder for icons to use for coloring options.

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ My version of the Etch-a-Sketc project for The Odin Project.
 Plans:
 
 1. Allow user to choose a color to sketch with
-2. Allow user to choose a gradient option to increase opacity with each mouseover
+2. Allow user to choose a gradient option to increase opacity with each mouseover?
 3. Allow user to choose a randomRGB option to have each cell result in a random color?
 4. Allow user to toggle cell borders on/off?
--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-function test() {
-  let selection = document.getElementsByName("option");
-  for (let i = 0, length = selection.length; i < length; i++) {
-    if (selection[i].checked) {
-      if (selection[i].value === "solid") {
-        alert("this will be a solid color");
-      }
-      break;
-    }
+const grid = document.getElementById("grid");
+const create = document.getElementById("create");
+let cells = document.querySelectorAll("cell");
+
+
+function createGrid() {
+  let i = 0;
+  grid.innerHTML = "";
+  size = Number(document.getElementById("size").value);
+  
+  if (!size || size < 2 || size > 100) return;
+
+  for (i = 0; i < (size ** 2); i++) {
+    let cell = document.createElement("div");
+    cell.className = "cell";
+    document.documentElement.style.setProperty("--cells", `${size}`);
+    grid.appendChild(cell);
   }
 }
 
-function test2() {
-  let choice = document.querySelector("select");
-  console.log(choice.value);
-}
+
+window.addEventListener("load", () => {
+  document.getElementById("size").value = 16;
+  createGrid();
+});
+
+create.addEventListener("click", createGrid);
