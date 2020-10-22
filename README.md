90e6b6895e31ecc8e9f781bb678c53e0d7fb54ba
thatblindgeye <thatblindgeye@gmail.com>
2020-10-22 17:10:28 -0400

    Add events for clearing grid and filling cells
    Added ability to clear a grid and fill cells with a selected color using
    color input. Included ability to use the project on computer or touchscreen
    device (will test to confirm it works).

--- README.md
+++ README.md
@@ -2,7 +2,8 @@ My version of the Etch-a-Sketc project for The Odin Project.
 
 Plans:
 
-1. Allow user to choose a color to sketch with
-2. Allow user to choose a gradient option to increase opacity with each mouseover?
-3. Allow user to choose a randomRGB option to have each cell result in a random color?
-4. Allow user to toggle cell borders on/off?
+1. Allow user to choose a color to sketch with DONE
+2. Allow user to choose a gradient option to increase opacity with each mouseover IN PROGRESS
+3. Allow user to have each cell be filled with a random color NOT STARTED
+4. Allow user to toggle cell borders on/off DONE
+5. Create shaking animation for grid when create/clear button is pressed NOT STARTED
--- script.js
+++ script.js
@@ -1,27 +1,117 @@
-const grid = document.getElementById("grid");
-const create = document.getElementById("create");
-let cells = document.querySelectorAll("cell");
+"use strict";
 
+const grid = document.getElementById("grid");
+const createBtn = document.getElementById("create");
+const clearBtn = document.getElementById("clear");
+const input = document.getElementById("size");
+let toggle = document.querySelector("#slider");
+let draw = false;
+let cells;
+let color;
 
 function createGrid() {
-  let i = 0;
+  let size = input.value;
   grid.innerHTML = "";
-  size = Number(document.getElementById("size").value);
-  
-  if (!size || size < 2 || size > 100) return;
+  draw = false;
+  document.documentElement.style.setProperty("--cells", `${size}`);
+  for (let i = 0; i < (size ** 2); i++) {
+    let newCell = document.createElement("div");
+    newCell.className = "cell";
+    grid.appendChild(newCell);
+  }
+  cells = document.getElementsByClassName("cell");
+}
 
-  for (i = 0; i < (size ** 2); i++) {
-    let cell = document.createElement("div");
-    cell.className = "cell";
-    document.documentElement.style.setProperty("--cells", `${size}`);
-    grid.appendChild(cell);
+function clearGrid() {
+  cells = Array.from(cells);
+  draw = false;
+  cells.forEach((cells) => {
+    cells.style.backgroundColor = "initial";
+  })
+}
+
+function toggleDraw(e) {
+  if (!draw) {
+    draw = true;
+  } else {
+    draw = false;
   }
 }
 
+function removeDrawToggle() {
+  window.removeEventListener("click", toggleDraw);
+}
+
+function chooseSolid(e) {
+  color = document.querySelector(".color-picker").value;
+  e.target.style.backgroundColor = `${color}`;
+}
+
+function chooseGradient(e) {
+  color = document.querySelector(".color-picker").value;
+  let currentColor = getComputedStyle(e.target).getPropertyValue("background-color");
+  let newColor = hexToRGB(color, 0.1);
+
+  e.target.style.setProperty("background-color", `${newColor}`);
+}
+
+function hexToRGB(hex, alpha) {
+  let r = parseInt(hex.slice(1, 3), 16),
+      g = parseInt(hex.slice(3, 5), 16),
+      b = parseInt(hex.slice(5, 7), 16);
+
+  return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+}
+
+function fillCells(e) {
+  if (e.target.className === "cell") {
+    if (draw === true) {
+      chooseSolid(e);
+    }
+  }
+}
+
+function toggleGrid() {
+  cells = Array.from(cells);
+
+  if (toggle.checked === true) {
+    cells.forEach((cells) => {
+      cells.style.setProperty("border", "var(--border-on)");
+    })
+  } else if (toggle.checked === false) {
+    cells.forEach((cells) => {
+      cells.style.setProperty("border", "var(--border-off)");
+    })
+  }
+}
 
 window.addEventListener("load", () => {
-  document.getElementById("size").value = 16;
+  input.value = 16;
+  toggle.checked = true;
   createGrid();
 });
 
-create.addEventListener("click", createGrid);
+createBtn.addEventListener("click", () => {
+  if (input.value < 2 || input.value > 100) return;
+  createGrid();
+});
+
+input.addEventListener("keydown", (e) => {
+  if (input.value < 2 || input.value > 100) return;
+  if (e.key === "Enter") {
+    createGrid();
+  }
+})
+
+clearBtn.addEventListener("click", clearGrid);
+
+grid.addEventListener("click", toggleDraw);
+
+grid.addEventListener("mousemove", fillCells);
+grid.addEventListener("touchstart", (e) => {
+  removeDrawToggle();
+  draw = true;
+  fillCells(e);
+});
+
+toggle.addEventListener("change", toggleGrid);
